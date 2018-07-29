137bbfc6479db6212c4fd19e973214013c1221ad
Ruslan Gainutdinov <huksley@sdot.ru>
2018-07-29 12:13:45 +0300

    Added config for CircleCI doc build

--- mkdocs.sh
+++ mkdocs.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
 #mvn -Pdoc-html
-git clone -b gh-pages https://github.com/huksley/spring-boot2-template.git gh-pages
+if [ ! -d gh-pages ]; then
+	git clone -b gh-pages https://github.com/huksley/spring-boot2-template.git gh-pages
+fi
 cp -rv target/generated-docs/* gh-pages/
 cd gh-pages
+
+git config --global user.email "cicd@ruslan.org"
+git config --global user.name "CICD"
 git add --ignore-errors *
 git commit -m "Updated documentation"
 git push origin gh-pages
