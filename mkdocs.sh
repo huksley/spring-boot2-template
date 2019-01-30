1c363698fb72df9bcba6ff28ba0d62d4c27cda56
CICD <cicd@ruslan.org>
2019-01-30 12:46:55 +0200

    Fixing CircleCI workflows

--- mkdocs.sh
+++ mkdocs.sh
@@ -2,7 +2,12 @@
 set -e
 mvn -Pdoc-html
 if [ ! -d gh-pages ]; then
-	git clone -b gh-pages git@github.com:huksley/spring-boot2-template.git gh-pages
+    if [ "$CI" == "true" ]; then
+        # Use ssh URI in CI environment
+        git clone -b gh-pages git@github.com:huksley/spring-boot2-template.git gh-pages
+    else
+        git clone -b gh-pages https://github.com/huksley/spring-boot2-template.git gh-pages
+    fi
 fi
 cp -rv target/generated-docs/* gh-pages/
 cd gh-pages
