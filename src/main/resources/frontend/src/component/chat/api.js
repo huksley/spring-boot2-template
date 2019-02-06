555019013e9652e24d7e5b9b6fe220ca9e1fc49f
Ruslan Gainutdinov <ruslan.gainutdinov@futurice.com>
2019-02-06 17:59:20 +0200

    Almost working WebSocket API implementation

--- src/main/resources/frontend/src/component/chat/api.js
+++ src/main/resources/frontend/src/component/chat/api.js
@@ -1,11 +1,93 @@
-//const WebSocketApi = {};
+import config from "../../config";
 
 let mockUsers = [];
 let mockListen = undefined;
+let socket = null;
+let socketConnected = false;
+let socketQueue = [];
+let messageListener = undefined;
+let usersListener = undefined;
+
+const WebSocketApi = {
+  open: afterOpen => {
+    console.log("open websocket api");
+    socket = new WebSocket(config.chatUrl);
+    socket.onopen = function() {
+      console.log("clientWebSocket.onopen", socket);
+      console.log("clientWebSocket.readyState", "websocketstatus");
+      afterOpen();
+      if (socketQueue.length > 0) {
+        let q = socketQueue;
+        socketQueue = [];
+        for (let i = 0; i < q.length; i++) {
+          socket.send(q[i]);
+        }
+      }
+    };
+
+    socket.onclose = function(data) {
+      console.log("clientWebSocket.onclose", socket, data);
+      socketConnected = false;
+    };
+    socket.onerror = function(error) {
+      console.log("clientWebSocket.onerror", socket, error);
+    };
+    socket.onmessage = function(msg) {
+      console.log("clientWebSocket.onmessage", socket, msg);
+      let message = JSON.parse(msg.data);
+      if (message) {
+        if (message.type === "message") {
+          if (messageListener !== undefined) {
+            messageListener(message.message);
+          }
+        } else if (message.type === "users") {
+          if (usersListener !== undefined) {
+            usersListener(message.users);
+          }
+        } else if (message.type === "getUsers") {
+          // Multicast ignore
+        } else if (message.type === "newUser") {
+          // Multicast ignore
+        } else {
+          console.warn("Unknown message type: ", message.type);
+        }
+      }
+    };
+  },
+  close: userName => {
+    console.log("close", userName);
+    socket.close();
+  },
+  newUser: userName => {
+    mockUsers.push(userName);
+    console.log("newUser", userName);
+    socket.send(JSON.stringify({ type: "newUser", userName: userName }));
+  },
+  listenMessage: handler => (messageListener = handler),
+  send: message => {
+    console.log("send", message);
+    if (socket !== null && socketConnected) {
+      socket.send(JSON.stringify({ type: "message", message: message }));
+    } else {
+      socketQueue.push(JSON.stringify({ type: "message", message: message }));
+    }
+  },
+  getUsers: handler => {
+    if (handler !== undefined) {
+      usersListener = handler;
+      if (socket != null && socketConnected) {
+        socket.send(JSON.stringify({ type: "getUsers" }));
+      } else {
+        socketQueue.push(JSON.stringify({ type: "getUsers" }));
+      }
+    }
+  }
+};
 
 const MockApi = {
-  open: () => {
-    console.log("open");
+  open: afterOpen => {
+    console.log("open mock api");
+    afterOpen();
   },
   close: userName => {
     console.log("close", userName);
@@ -27,4 +109,7 @@ const MockApi = {
   }
 };
 
-export const api = MockApi;
+export const api =
+  config.chatMockApi === undefined || config.chatMockApi === true
+    ? MockApi
+    : WebSocketApi;
