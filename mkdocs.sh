0e4f8fe7b522ca9091d852fd8227c1b6e8d6accc
Ruslan Gainutdinov <ruslan.gainutdinov@futurice.com>
2019-02-06 14:52:48 +0200

    Locally setting user and email for git

--- mkdocs.sh
+++ mkdocs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 mvn -Pdoc-html
+
 if [ ! -d gh-pages ]; then
     if [ "$CI" == "true" ]; then
         # Use ssh URI in CI environment
@@ -9,14 +10,17 @@ if [ ! -d gh-pages ]; then
         git clone -b gh-pages https://github.com/huksley/spring-boot2-template.git gh-pages
     fi
 fi
+
 cp -rv target/generated-docs/* gh-pages/
 cd gh-pages
 
-git config --global user.email "cicd@ruslan.org"
-git config --global user.name "CICD"
+# Locally set CICD user
+git config --local user.email "cicd@ruslan.org"
+git config --local user.name "CICD"
 git add --ignore-errors *
 git commit -m "Updated documentation [skip ci]"
 git push origin gh-pages
 cd ..
 rm -rf gh-pages target
+
 
