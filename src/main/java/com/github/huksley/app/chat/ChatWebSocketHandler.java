65b6d75ad249d226f0e390e29a65d6fe1ae0ce28
Ruslan Gainutdinov <ruslan.gainutdinov@futurice.com>
2019-02-07 10:27:35 +0200

    Fixed message being queued instead of sending

--- src/main/java/com/github/huksley/app/chat/ChatWebSocketHandler.java
+++ src/main/java/com/github/huksley/app/chat/ChatWebSocketHandler.java
@@ -4,10 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.WebSocketMessage;
-import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.*;
 
 import java.io.IOException;
 
@@ -30,7 +27,7 @@ public class ChatWebSocketHandler implements WebSocketHandler {
         users.forEach((s) -> {
             try {
                 log.info("Sending {} to {}", webSocketMessage, s);
-                s.sendMessage(webSocketMessage);
+                s.sendMessage(new TextMessage((String) webSocketMessage.getPayload()));
             } catch (IOException e) {
                 log.warn("Failed to send message {} to {}: ", webSocketMessage, s, e);
             }
@@ -39,7 +36,7 @@ public class ChatWebSocketHandler implements WebSocketHandler {
 
     @Override
     public void handleTransportError(WebSocketSession webSocketSession, Throwable throwable) {
-        log.info("handleTransportError, errror {}", throwable);
+        log.info("handleTransportError, error {}", throwable);
     }
 
     @Override
