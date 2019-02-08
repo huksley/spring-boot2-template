edf21a7a2c3ea2ef25487eea333b72018cd453c9
Ruslan Gainutdinov <ruslan.gainutdinov@futurice.com>
2019-02-08 11:24:45 +0200

    Proper ENV var REACT_APP_CHAT_URL for ReactJS

--- src/main/resources/frontend/src/config.js
+++ src/main/resources/frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
     chatMockApi: process.env.REACT_APP_CHAT_MOCK_API !== undefined ? process.env.REACT_APP_CHAT_MOCK_API : undefined,
-    chatUrl: process.env.CHAT_URL !== undefined ? process.env.CHAT_URL : "ws://localhost:3000/api/chat/events",
+    chatUrl: process.env.REACT_APP_CHAT_URL !== undefined ? process.env.REACT_APP_CHAT_URL : "ws://localhost:3000/api/chat/events",
     dev: process.env.NODE_ENV !== 'production',
     production: process.env.NODE_ENV === 'production',
     offlineFirst: process.env.REACT_APP_OFFLINE_FIRST !== undefined ? process.env.REACT_APP_OFFLINE_FIRST : false
